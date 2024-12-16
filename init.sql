e452c2a2e6e1c8b12eba11dcba01b04670bd947e
Test User <danya_zav@test.com>
2024-12-16 18:23:50 +0300

    do all

--- init.sql
+++ init.sql
@@ -9,18 +9,26 @@ create table accounts
 	login varchar(255) not null,
 	balance bigint default 0 not null,
 	created_at timestamp default now()
-) ENGINE = INNODB
-CHARACTER SET utf8
-COLLATE utf8_unicode_ci;
+);
 
 insert into accounts (login, balance) values ('petya', 1000);
 insert into accounts (login, balance) values ('vasya', 2000);
 insert into accounts (login, balance) values ('mark', 500);
 
 ----Read uncommitted
+-- Read_uncommitted_1
 ---- см инструкцию по сайту
---INSERT INTO accounts (login, balance, created_at) VALUES('ivan', 750, NOW());
---DELETE FROM accounts WHERE login = 'vasya';
---UPDATE accounts SET balance = 1500 WHERE login = 'petya';
+INSERT INTO accounts (login, balance, created_at) VALUES('ivan', 750, NOW());
+DELETE FROM accounts WHERE login = 'vasya';
+UPDATE accounts SET balance = 1500 WHERE login = 'petya';
 --
 --SELECT SUM(balance) FROM accounts;
+
+-- Read_committed_1
+--Repeatable_read_1
+UPDATE accounts SET balance = 2000 WHERE login = 'petya';
+
+drop table accounts;
+
+--Serializable
+UPDATE accounts SET balance = 1000 WHERE login = 'petya';
